7396a87e86f7445c63107198bb316b0f75fc2086
unknown <felipe.santos.ofic@gmail.com>
2021-09-01 14:55:07 -0300

    Front End

---
+++ Front-End/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent},
+
+  //{path: 'inicio', component: InicioComponent},
+  //{path: 'tema', component: TemaComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Front-End/src/app/app.module.ts
@@ -0,0 +1,40 @@
+//import { CadastrarComponent } from './cadastrar/cadastrar.component';
+//import { EntrarComponent } from './entrar/entrar.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SobreNosComponent,
+    HomeComponent,
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
